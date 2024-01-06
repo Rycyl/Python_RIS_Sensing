3006dcbd60d668d08670319e4487819778c99e19
DawidBrzaki <145806899+DawidBrzaki@users.noreply.github.com>
2024-01-06 09:37:22 +0100

    Add files via upload
    analyzer.py - added angle information to trace file
    sterowanie.py - left rotation etc.

--- analyzer.py
+++ analyzer.py
@@ -60,7 +60,7 @@ def meas_prep(freq : int, span : int, mode : str, revlevel : int, rbw : str):
     
 
 
-def trace_get():
+def trace_get(az_angle,el_angle):
     """Initialize continuous measurement, stop it after the desired time, query trace data"""
     analyzer.write_str_with_opc('INITiate:CONTinuous ON')  # Continuous measurement on trace 1 ON
     print('Please wait for maxima to be found...')
@@ -89,6 +89,7 @@ def trace_get():
             max_amp = amp
             max_x = x
         x = x+1
+    file.write(f'Az_angle= {az_angle},El_angle= {el_angle} ')  # Write kąty 
     file.write(f'{(start_freq + max_x * step_size):.1f}')  # Write adequate frequency information
     file.write(";")
     file.write(f'{max_amp:.2f}')  # Write adequate amplitude information
@@ -96,10 +97,12 @@ def trace_get():
     file.close()  # CLose the file
     
 if __name__ == "__main__":
+    az_angle=0
+    el_angle=10
     com_prep()
     com_check()
     meas_prep(28E9, 100E3, "MAXHold ", -30, "500 Hz")
-    trace_get()
+    trace_get(az_angle,el_angle)
     close()
     print('Program successfully ended.')
     print('Wrote trace data into', TRACE_FILE)
