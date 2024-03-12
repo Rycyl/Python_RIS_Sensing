04897a8b6f108f8084fabd8f38bca940e560bd2e
mgarczyk <madzelsky@gmail.com>
2024-03-12 13:06:15 +0100

    Changes in config. Shorter time of measure.

--- RIS_usb.py
+++ RIS_usb.py
@@ -6,6 +6,7 @@ try:
     with open ("config.json") as config_f:
        config = json.load(config_f)
        ris_port = config["RIS_PORT"]
+       ris_set_time = config["RIS_SET_TIME"]
        ris = Serial(ris_port, 115200)
 except FileNotFoundError:
     print("Brak pliku konfiguracyjnego.")
@@ -13,7 +14,7 @@ except FileNotFoundError:
 
 def reset_RIS():
     ris.write(bytes('!Reset\n', 'utf-8'))
-    time.sleep(1)
+    time.sleep(ris_set_time)
     while ris.in_waiting:
         response = ris.readline().decode('utf-8').rstrip()
         print(f"Response from resetting RIS: {response}")
@@ -22,13 +23,13 @@ def reset_RIS():
 def set_BT_key(key : str):
     ris.write(bytes(f'!BT-Key={key}', 'utf-8'))
     # Wait long enough or check ris.NumBytesAvailable for becoming non-zero
-    time.sleep(5)
+    time.sleep(2)
     response = ris.readline().decode('utf-8').rstrip()
     print(f"Response from setting a new Static Pass Key: {response}")
     
 def set_pattern(pattern):
     ris.write(bytes(f"!{pattern}\n", 'utf-8'))
-    time.sleep(0.5)
+    time.sleep(ris_set_time)
     
 def read_EXT_voltage() -> float:
     ris.write(bytes('?Vext\n', 'utf-8'))
@@ -38,11 +39,11 @@ def read_EXT_voltage() -> float:
 
 def read_pattern():
     ris.write(bytes('?Pattern\n', 'utf-8'))
-    time.sleep(1)
+    time.sleep(ris_set_time)
     while ris.in_waiting:
         response = ris.readline().decode('utf-8').rstrip()
         print(f"Response from resetting RIS: {response}")
-        time.sleep(0.1)
+        time.sleep(ris_set_time)
         
             
 
