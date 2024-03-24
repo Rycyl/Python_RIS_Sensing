63801644a4629ab5fa5bfa2804a8c5e50089fff8
TreeCode <t.jaworski0011@gmail.com>
2024-03-24 15:12:26 +0100

    Update plt_plot.py
    Dodano wykresy min/max
    Znaleziono buga - all elements on i first on mają takie same wykresy (dane się różnią, trza znaleźć o co biega, na razie nie wiem)

--- Skrypty_do_wynikow/plt_draw.py
+++ Skrypty_do_wynikow/plt_draw.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import os
 import os.path
 import sys
-
+import numpy as np
+import copy
 #name_of_script = sys.argv[0]
 folder = sys.argv[1]
 file = sys.argv[2]
@@ -15,7 +16,7 @@ file_path = os.path.join(data_folder, file)
 class baza: #klasa przechowuje poukładane pomiary wg patternów na RIS
         def __init__(self, pattern,fq,lvl):
             self.pattern = pattern
-            print("Added measurements of:: ", self.pattern)
+            #print("Added measurements of:: ", self.pattern)
             self.measures = [[fq],[lvl]]
             self.mean_pow = 0
         
@@ -26,46 +27,105 @@ class baza: #klasa przechowuje poukładane pomiary wg patternów na RIS
         def mean_power(self):
             sum = 0
             n = 0
-            for i in self.measures:
-                sum += i[1]
+            #print(self.measures)
+            for i in self.measures[1]:
+                #print("i::",i)
+                sum += i
                 n+=1
             self.mean_pow = sum/n
             return
 
 data = []
-
+mes_max=-100.0
+mes_min=0
 with open(file_path, 'r') as file:
     csv_reader = csv.reader(file, delimiter=';')
+    
     next(csv_reader)  # Pominięcie nagłówka, jeśli istnieje
     for row in csv_reader:
+        #print(row)
         #print("ROW", row[0])
         if len(row) < 3: #jezeli wiersz jest za krotki pomin
             continue
         found = False
         for i in data:
+            if float(row[2])>mes_max:
+                mes_max=float(row[2])
+            if float(row[2])<mes_min:
+                mes_min=float(row[2])
             if (row[0] == i.pattern) :
                 i.add_measure(float(row[1]), float(row[2]))  # Konwertowanie na float + wpisanie do obiektu wynikow
                 found = True
                 break
         if not found:
-            x = baza(row[0], float(row[1]), float(row[2]))
-            data.append(x)
+            data.append(baza(row[0], float(row[1]), float(row[2])))
 
 for i in data:
     i.mean_power()
 
-#print(data)
-# Wykres
+
+
+# Wykresy normalne
 for i in range(5):
-    fig, ax = plt.subplots(figsize=(8, 6))
+    fig, ax = plt.subplots(figsize=(15, 12))
 
     for item in data[i*5:i*5+5]:
         ax.plot(item.measures[0], item.measures[1], label=item.pattern)
-    ax.set_xlabel('Częstotliwość')
-    ax.set_ylabel('Poziom mocy (dBm)')
+    ax.set_xlabel('Częstotliwość [Hz]')
+    ax.set_ylabel('Poziom mocy [dBm]')
     ax.grid()
+    ax.set_ylim(mes_min-1, mes_max+1)
     fig.legend()
     # Tworzenie nazwy pliku
     file_name = os.path.splitext(file_path)[0] + "_wykres" + str(i+1) + ".png"
     plt.savefig(file_name)
     plt.close()
+
+##poziomy średnie
+fig, ax = plt.subplots(figsize=(15, 12))
+y=[[],[]]
+for item in data:
+    y[0].append(item.mean_pow)
+    #print(item.mean_pow)
+    y[1].append(item.pattern)
+y_pos = np.arange(len(y[1]))
+min=min(y[0])
+max=max(y[0])
+ax.barh(y_pos, y[0], align='center')
+ax.set_xlabel('Średni poziom mocy [dBm]')
+ax.set_yticks(y_pos, labels=y[1])
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlim(min-1, max + 5)
+plt.tight_layout()
+file_name = os.path.splitext(file_path)[0] + "_mean_pow" + ".png"
+plt.savefig(file_name)
+plt.close()
+    
+    
+#min/max plot points
+x = data[0].measures[0]
+max_handle = data[0].measures[1]
+min_handle = copy.deepcopy(max_handle)
+fig, ax = plt.subplots(figsize=(15, 12))
+print(max_handle, id(max_handle))
+print(min_handle, id(min_handle))
+for i in data[1:]:
+    for j in range(0,(len(i.measures[1]))):
+    
+        if(max_handle[j]<i.measures[1][j]):
+            max_handle[j]=i.measures[1][j]
+            
+        if(min_handle[j]>i.measures[1][j]):
+            min_handle[j]=i.measures[1][j]
+            
+print(max_handle)
+print(min_handle)
+ax.plot(x, max_handle, label="maksima znalezione")
+ax.plot(x, min_handle, label="minima znalezione")
+ax.set_xlabel('Częstotliwość [Hz]')
+ax.set_ylabel('Poziom mocy [dBm]') 
+ax.grid()
+fig.legend()
+file_name = os.path.splitext(file_path)[0] + "_min_max" + ".png"
+plt.savefig(file_name)
+plt.close()
