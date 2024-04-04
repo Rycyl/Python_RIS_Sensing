0342a64084e151df72eef2db8c1f875f312baa62
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2024-04-04 17:40:27 +0200

    Changes with counting angle of measurment.

--- RIS_moving_measurment.py
+++ RIS_moving_measurment.py
@@ -14,14 +14,15 @@ try:
         start_freq=config["START_FREQ"]
         end_freq=config["END_FREQ"]
         step_freq=config["STEP_FREQ"]
-        motor_step=config["MOTOR_STEP"]
+        motor_step=config["MOTOR_STEPS"]
         step_resolution = config["STEP_RESOLUTION"]
-        number_of_angles = config[]
+        number_of_angles = config["NUMBER_OF_ANGLES"]
         span=config["SPAN"]
         analyzer_mode=config["ANALYZER_MODE"]
         revlevel=config["REVLEVEL"]
         rbw=config["RBW"]
         generator_amplitude=config["GENERATOR_AMPLITUDE"]
+        steps_from_start = 0 # counts how many steps remote head did. Could be used to count actual measurement angle.
         # More modes will be add later.
         if config["GENERATOR_MODE"] == "CW":
             generator_mode = enums.FreqMode.CW
@@ -41,7 +42,7 @@ except FileNotFoundError:
     exit()
     
 def count_angle(step):
-    angle = step*1/(step_resolution)
+    angle = step*1/(step_resolution)*1.8
     return str(angle)
     
 def pattern_loop(freq, angle):
@@ -60,11 +61,13 @@ def freq_loop(freq_data, angle):
         generator.meas_prep(True, generator_mode, generator_amplitude, freq) # True means that generator is set up an generate something.
         pattern_loop(freq, angle)
         
-def angle_loop():
-    for angle in range(number_of_angles):
-        angle = count_angle(motor_step)
-        remote_head.obrot_prawo(motor_step) # move few steps to the right (descroption in config file)
+def angle_loop(freq_data):
+    for i in range(number_of_angles):
+        angle = count_angle(steps_from_start)
         freq_loop(freq_data, angle)
+        remote_head.obrot_prawo(motor_step) # move few steps to the right (descroption in config file)
+        steps_from_start += motor_step
+       
 
     
 if __name__=="__main__":
@@ -75,7 +78,7 @@ if __name__=="__main__":
         remote_head.az360()
         RIS_usb.reset_RIS()
         freq_data = np.arange(start_freq, end_freq, step_freq)
-        angle_loop(motor_step, freq_data)
+        angle_loop(freq_data)
         analyzer.meas_close()
         generator.meas_close()
         exit()
