5cae91bcb410d9e116956569e6e400cc19660db2
Rycyl <118639893+Rycyl@users.noreply.github.com>
2024-05-16 02:20:09 +0200

    dodatkowy wykres czy cos

--- Skrypty_sensing/test.py
+++ Skrypty_sensing/test.py
@@ -1,2 +1,16 @@
-import time
-print(time.ctime(time.time()))
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Sample data
+data = [-50, -60, -70, -45, -55]
+
+data = sorted(data, reverse=True)
+# Setting up the plot
+plt.figure(figsize=(8, 6))
+plt.bar(range(len(data)), np.abs(data), bottom=-80)
+
+# Setting y-axis limits
+#plt.ylim(-80, 0)
+#plt.gca().invert_yaxis()
+# Showing the plot
+plt.show()
