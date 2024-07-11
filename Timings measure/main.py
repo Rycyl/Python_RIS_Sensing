f2416848fbacc5926789ff3555037b567beefaf6
treecodeXXI <132903579+treecodeXXI@users.noreply.github.com>
2024-07-11 14:20:39 +0200

    Add timing directory

---
+++ Timings measure/main.py
@@ -0,0 +1,23 @@
+import analyzer_sensing
+import generator
+from RIS import RIS
+from RsSmw import *
+import search_patterns
+
+if __name__ == "__main__":
+    ##time.sleep(20)
+    RIS = RIS(port='/dev/ttyUSB0')
+
+    RIS.reset()
+    generator.com_check()
+    analyzer_sensing.com_prep()
+    analyzer_sensing.com_check()
+    i = 0
+    brekpoint = 1
+    while(i<brekpoint):
+        i+=1
+        print("Iteration: ", i)
+        print(search_patterns.find_best_pattern_element_wise(RIS, mask='0b1'))
+
+    analyzer_sensing.meas_close()
+    generator.meas_close()
