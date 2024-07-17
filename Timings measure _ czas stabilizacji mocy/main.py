0b5649a24a545825377e735fb57a423165a0a2de
treecodeXXI <132903579+treecodeXXI@users.noreply.github.com>
2024-07-17 14:48:58 +0200

    Update

--- Timings measure _ czas stabilizacji mocy/main.py
+++ Timings measure _ czas stabilizacji mocy/main.py
@@ -10,9 +10,9 @@ config = Config()
 
 if __name__ == "__main__":
     ##time.sleep(20)
-    RIS = RIS(port='/dev/ttyUSB0')
+    #RIS = RIS(port='/dev/ttyUSB0')
     sweeptime_l = 0.001
-    RIS.reset()
+    #RIS.reset()
     generator.com_check()
     analyzer_sensing.com_prep()
     analyzer_sensing.com_check()
@@ -21,11 +21,12 @@ if __name__ == "__main__":
 
     
     
-    while(sweeptime_l < 0.1):
+    while(sweeptime_l < 0.6):
         i = 0
 
         with open(config.trace_file, 'a+') as file:
-                file.write("Pattern ID1 [dBm]" + "," + "Pattern ID3 [dBm]" + ",")
+                file.write("Połączenie kablem we-wy" + ',')
+                #file.write("Pattern ID1 [dBm]" + "," + "Pattern ID3 [dBm]" + ",")
                 file.write(str(sweeptime_l) + ',')
                 file.write('\n')
                 file.close
@@ -33,9 +34,9 @@ if __name__ == "__main__":
         while(i<brekpoint): #100 pomiarów na jednym sweeptime
             i+=1
             print("Iteration: ", i)
-            RIS.set_pattern("0x0000000000000000000000000000000000000000000000000000000000000000")
+            #RIS.set_pattern("0x0000000000000000000000000000000000000000000000000000000000000000")
             p1 = analyzer_sensing.trace_get_mean()
-            RIS.set_pattern("0x8000000000000000000000000000000000000000000000000000000000000000")
+            #RIS.set_pattern("0x8000000000000000000000000000000000000000000000000000000000000000")
             p2 = analyzer_sensing.trace_get_mean()
             with open(config.trace_file, 'a+') as file:
                 file.write(str(p1) + "," + str(p2) + ",")
