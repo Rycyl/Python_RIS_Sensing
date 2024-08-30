223e6c5b0317ef237d6d5ef613b6ebecab2135c5
TreeCode <t.jaworski0011@gmail.com>
2024-08-30 19:47:02 +0200

    Update main.py

--- Find_best_pattern/main.py
+++ Find_best_pattern/main.py
@@ -1,40 +1,23 @@
-import analyzer_sensing
-import generator
-from RIS import RIS
-#from RsSmw import *
-import search_patterns
-import numpy as np
-from file_writer import file_write_single_power
+from analyzer_sensing import Analyzer
+from generator import Generator
 from config_obj import Config
+from RIS import RIS
+import search_patterns
+
+import numpy as np
+
 from time import time, ctime, sleep
 from bitstring import Bits, BitArray, BitStream, pack
 
 config = Config()
 
 if __name__ == "__main__":
-    
+    analyzer = Analyzer(config)
+    generator = Generator(config)
     RIS = RIS(port='/dev/ttyUSB0')
     RIS.reset()
-    generator.com_check()
-    analyzer_sensing.com_prep()
-    analyzer_sensing.com_check()
-    #sleep(120)
-    '''
-    search_patterns.find_best_pattern_element_wise_by_group_measures(RIS, config, 1, find_min=False, debug=True, trace_file='trace_file_group_1.csv')
-    search_patterns.find_best_pattern_element_wise_by_group_measures(RIS, config, 2, find_min=False, debug=True, trace_file='trace_file_group_2.csv')
-    search_patterns.find_best_pattern_element_wise_by_group_measures(RIS, config, 4, find_min=False, debug=True, trace_file='trace_file_group_44.csv')
-    
-<<<<<<< Updated upstream
-    search_patterns.find_best_pattern_element_wise_by_group_measures(RIS, config, 1, find_min=False, debug=True, trace_file='trace_file_group_11.csv')
-    search_patterns.find_best_pattern_element_wise_by_group_measures(RIS, config, 2, find_min=False, debug=True, trace_file='trace_file_group_22.csv')
-    search_patterns.find_best_pattern_element_wise_by_group_measures(RIS, config, 4, find_min=False, debug=True, trace_file='trace_file_group_44.csv')
-    '''
-    RIS.set_pattern('0x00f807f009f804f801f802f80bfe007e01f80af803fc04fe01fe42f025d0c17c')
 
-    search_patterns.find_best_pattern_element_wise_by_group_measures(RIS, config, 4, find_min=False, debug=True, trace_file='trace_file_group_444444.csv')
-=======
     search_patterns.find_best_pattern_element_wise_by_group_measures(RIS, generator, analyzer, config, 4, FIND_MIN=False, DEBUG_FLAG=True, TRACE_FILE='trace_file_group_4.csv')
->>>>>>> Stashed changes
 
     generator.close()
     analyzer_sensing.close()
