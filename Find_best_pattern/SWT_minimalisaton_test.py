147fc6a33a58857e6a167a85dde83a38c5f0f00b
treecodeXXI <132903579+treecodeXXI@users.noreply.github.com>
2024-09-04 16:35:31 +0200

    Kolenje pomiary

--- Find_best_pattern/SWT_minimalisaton_test.py
+++ Find_best_pattern/SWT_minimalisaton_test.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     x = 0
     sigma = 3
-    '''
+    
     config.update_swt(0.1)
     sleep(30)
     search_patterns.find_best_pattern_element_wise(RIS=ris, ANALYZER=analyzer, GENERATOR=generator, CONFIG=config)
@@ -34,12 +34,12 @@ if __name__ == "__main__":
             sigma = 2
         else:
             sigma = 3
-        search_patterns.find_best_pattern_element_wise_by_group_measures(ris, generator, analyzer, config, 1, N_SIGMA=sigma ,MEASURE_FILE= f'find_best_pattern_element_wise_by_group_measures_swt_test_{x}.csv' , FIND_MIN=False, DEBUG_FLAG=True, TRACE_FILE=f'trace_file_group_swt_test{x}.csv', time_safety_margin = swt_val)
+        search_patterns.find_best_pattern_element_wise_by_group_measures(ris, generator, analyzer, config, 4, N_SIGMA=sigma, STD_CHECK_ON=False ,MEASURE_FILE= f'find_best_pattern_element_wise_by_group_measures_swt_test_v2_{x}.csv' , FIND_MIN=False, DEBUG_FLAG=True, TRACE_FILE=f'trace_file_group_swt_test{x}.csv', time_safety_margin = swt_val)
         x += 1
 
     config.update_swt(0.1)
     search_patterns.find_best_pattern_element_wise(RIS=ris, ANALYZER=analyzer, GENERATOR=generator, CONFIG=config)
-    '''
+    
     plot_trace.run_all()
     
     generator.close()
