6abe2dddf38f338589bdeb031ecaa7f70459315d
treecodeXXI <____>
2024-11-15 16:15:29 +0100

    aaaa

--- Find_best_pattern/monopol_test.py
+++ Find_best_pattern/monopol_test.py
@@ -16,37 +16,11 @@ if __name__ == "__main__":
     ris = RIS(port='/dev/ttyUSB0')
     ris.reset()
     config = Config()
-    analyzer = Analyzer(config)
-    generator = Generator(config)
+    analyzer = Analyzer(config, phy_device=False)
+    generator = Generator(config, phy_device=False)
+    start_pat = BitArray(hex='F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0')
 
-    mes_file_name = 'anntena_test_14_11_omni_no_LOS_pos_2'
-    sleep(60)
-    for i in range(1, 8):
-        mes_file = mes_file_name + '_' + str(i) + '.csv'
-
-        best_pattern, best_power = search_patterns.find_best_pattern_element_wise(ris, generator, analyzer, config, MEASURE_FILE =  mes_file, FIND_MIN = False)
-
-        with open(mes_file, 'a') as file:
-            file.write('best pattern')
-            file.write('\n')
-            file.write(str(best_power) + ",")
-            file.write(str(best_pattern))
-            file.write('\n')
-            file.close()
-
-        mes_file_min = mes_file[:-4] + '_min.csv'
-
-        best_pattern_min, best_power_min = search_patterns.find_best_pattern_element_wise(ris, generator, analyzer, config, MEASURE_FILE =  mes_file_min, FIND_MIN = True)
-
-        with open(mes_file_min, 'a') as file:
-            file.write('best pattern')
-            file.write('\n')
-            file.write(str(best_power_min) + ",")
-            file.write(str(best_pattern_min))
-            file.write('\n')
-            file.close()
-        sleep(600) # 10 min
-        #break
+    best_pattern_min, best_power_min = search_patterns.find_best_pattern_element_wise(ris, generator, analyzer, config, MASK="0b1010")
 
     generator.close()
     analyzer.close()
