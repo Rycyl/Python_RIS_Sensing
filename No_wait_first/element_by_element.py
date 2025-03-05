7bad60dae10897cd3abb4397110afeefc80ed5c3
Rycyl <118639893+Rycyl@users.noreply.github.com>
2025-03-05 16:50:14 +0100

    pomiar_05_03

--- No_wait_first/element_by_element.py
+++ No_wait_first/element_by_element.py
@@ -25,6 +25,61 @@ class sing_pat_per_run():
         with open(codebook, "r") as f:
             lines = f.readlines()
             for line in lines:
+                pattern, angle = line.split(";")
+                datum = (BitArray(hex=pattern), angle.strip("\n"), "NaN")
+                codes.append(datum)
+                #codes.append(BitArray(hex=line))
+            f.close()
+        return codes
+    
+    def do_measure(self):
+        self.Mes_pow = self.Anal.trace_get_mean()
+        return self.Mes_pow
+    
+    def start_measure(self):       
+        self.All_measured = []
+        for datum in self.Codebook:
+            Do_Measure = threading.Thread(target = self.do_measure)
+            Do_Measure.start()
+            # print(pattern)
+            # print(pattern.hex)
+            # print("settin pattern")
+            #t_1 = time.time()
+            self.Ris.set_pattern('0x' + datum[0].hex)
+            # print(time.time() - t_1)
+            # print("pattern set")
+            Do_Measure.join()
+            self.All_measured.append((datum[0],datum[1],self.Mes_pow))
+        return self.save_to_file()
+    
+    def save_to_file(self):
+        with open(self.file, 'w+') as csvfile:
+            csvfile.write("Pattern, Angle, Power")
+            csvfile.write("\n")
+            for datum in self.All_measured:
+                text = f"{datum[0]}; {datum[1]}; {datum[2]}"
+                csvfile.write(text + "\n")
+            csvfile.close()
+        return self.All_measured
+    
+
+class sing_pat_per_run_w_wait():
+    def __init__(self, ris: RIS, anal: Analyzer, gen: Generator, exit_file: str, codebook: str):
+        self.Ris = ris
+        self.Anal = anal
+        self.Gen = gen
+        self.file = exit_file
+        self.Codebook = self.load_code_book(codebook)
+        self.No_of_pats = len(self.Codebook)
+        self.Mes_pow = None
+        self.All_measured = {}
+
+
+    def load_code_book(self, codebook):
+        codes = []
+        with open(codebook, "r") as f:
+            lines = f.readlines()
+            for line in lines:
                 codes.append(BitArray(hex=line))
             f.close()
         return codes
@@ -36,16 +91,17 @@ class sing_pat_per_run():
     def start_measure(self):       
         self.All_measured = {}
         for pattern in self.Codebook:
-            Do_Measure = threading.Thread(target = self.do_measure)
-            Do_Measure.start()
-            print(pattern)
-            print(pattern.hex)
-            print("settin pattern")
-            t_1 = time.time()
+            # Do_Measure = threading.Thread(target = self.do_measure)
+            # Do_Measure.start()
+            # print(pattern)
+            # print(pattern.hex)
+            # print("settin pattern")
+            # t_1 = time.time()
             self.Ris.set_pattern('0x' + pattern.hex)
-            print(time.time() - t_1)
+            self.do_measure()
+            # print(time.time() - t_1)
             print("pattern set")
-            Do_Measure.join()
+            # Do_Measure.join()
             self.All_measured['0x' + pattern.hex] = self.Mes_pow
         return self.save_to_file()
     
--- No_wait_first/main.py
+++ No_wait_first/main.py
@@ -8,6 +8,7 @@ import time
 
 
 if __name__ == "__main__":
+    t_1 = time.time()
     Conf = Config()
     phy_device_input = True
 
@@ -15,6 +16,10 @@ if __name__ == "__main__":
     generator = Generator(Conf, phy_device_input)
     ris = RIS(port='/dev/ttyUSB0', phy_device=phy_device_input)
     print("RIS done")
+    generator.meas_prep(True, Conf.generator_mode, Conf.generator_amplitude, Conf.freq)
+    analyzer.meas_prep(Conf.freq, Conf.sweptime, Conf.span, Conf.analyzer_mode, Conf.detector, Conf.revlevel, Conf.rbw, Conf.swepnt)
+    # GENERATOR.meas_prep(True, Conf.generator_mode, Conf.generator_amplitude, Conf.freq)
+    # ANALYZER.meas_prep(Conf.freq, Conf.sweptime, Conf.span, Conf.analyzer_mode, Conf.detector, Conf.revlevel, Conf.rbw, Conf.swepnt)
 
     meas_file_name = "test"
     code_book_file = "Codebook.csv"
@@ -23,8 +28,8 @@ if __name__ == "__main__":
     print("create file done")
     meas_obj = sing_pat_per_run(ris, analyzer, generator, meas_file, code_book_file)
     print("obj done")
-
+    print(time.time() - t_1)
     #time.sleep(10) time for evacutation
     meas_obj.start_measure()
-
+    print(time.time() - t_1)
     exit()
