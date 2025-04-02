d2c6fcce1e5b751a0165837ea60a36a097058eec
Rycyl <118639893+Rycyl@users.noreply.github.com>
2025-04-02 12:36:00 +0200

    Updates for uwb

--- No_wait_first/get_angle.py
+++ No_wait_first/get_angle.py
@@ -1,59 +1,81 @@
 from math import acos, sqrt, cos, pi, degrees
 from get_distances import UWB_module
+import os
+from time import sleep
 
 class Antenna_Geometry():
-    def __init__(self, Anchor: UWB_module, ris_dist):
+    def __init__(self, Anchor: UWB_module, ris_dist): #RIS_DIST IN METERS
         self.Anchor = Anchor
         self.ris_dist = ris_dist*1000
+        self.b = self.ris_dist
+        self.half_of_b = self.ris_dist/2
         self.Anchor_dists = None
         self.Tag_dist = None
     
     def calc_alpha(self, a, c):
-        '''Alpha czyli kąt między RIS a dystansem od Anchora 0 do Anchora 1
-        a to właśnie dsystans A0 do A1, a c to dystans A1 do A2'''
-        b = self.ris_dist
-        alpha_arg = (a**2 + b**2 -c**2)/(2*a*b)
+        ''' Alpha - kąt między RIS a A0-A1
+            a == A0 do A1,
+            c == A1 do A2        '''
+        alpha_arg = (a**2 + self.b**2 -c**2)/(2*a*self.b)
+        # print("alfa")
         return acos(alpha_arg)
 
     def calc_beta(self, x, y):
-        '''Beta czyli kąt między RIS a odcinkiem A2 do T (czyli x) y to odcinek A0 do T'''
-        b = self.ris_dist
-        beta_arg = (x**2 + b**2 - y**2)/(2*x*b)
+        ''' Beta kąt między RIS a A2-T
+            x == A2 do T
+            y == A0 do T        '''
+        beta_arg = (x**2 + self.b**2 - y**2)/(2*x*self.b)
+        # print("beta")
         return acos(beta_arg)
     
-    def calc_gamma(self, x, b, beta):
+    def calc_m(self, x, b, beta):
         return sqrt(x**2 + b**2 - 2*x*b*cos(beta))
     
-    def calc_kappa(self, a, b, alpha):
+    def calc_n(self, a, b, alpha):
         return sqrt(a**2 + b**2 - 2*a*b*cos(alpha))
     
-    def calc_theta(self, x, b, gamma):
-        theta_arg = (gamma**2 + b**2 - x**2)/(2*gamma*b)
+    def calc_theta(self, x, b, m):
+        theta_arg = (m**2 + b**2 - x**2)/(2*m*b)
+        # print("teta")
         return acos(theta_arg)
     
-    def calc_omega(self, a, b, kappa):
-        omega_arg = (kappa**2 + b**2 - a**2)/(2*kappa*b)
+    def calc_omega(self, a, b, n):
+        omega_arg = (n**2 + b**2 - a**2)/(2*n*b)
+        # print("omega")
         return acos(omega_arg)
     
     def get_distances(self):
         self.Tag_dist, self.Anchor_dists = self.Anchor.get_distances()
         return
     
-    def get_angles(self):
+    def calc_triangle_side_opposite_angle(a, b, alpha):
+        return sqrt(a**2 + b**2 - 2*a*b*cos(alpha))
+
+    def calc_triangle_angle(a,b,c):
+        arg = (a**2 + b**2 - c**2)/(2*a*b)
+        return acos(arg)
+
+    def get_angles(self, Print_vals = False):
+        '''
+        DISTANCES ARE IN mm BY DEFAULT
+        ANGLES IN RADIANS        
+        '''
         self.get_distances()
-        half_of_b = self.ris_dist/2
         a = self.Anchor_dists[1]
         c = self.Anchor_dists[3]
         y = self.Tag_dist[0]
         x = self.Tag_dist[2]
-        alpha = self.calc_alpha(a, c)
-        beta = self.calc_beta(x, y)
-        gamma = self.calc_gamma(x, half_of_b, beta)
-        kappa = self.calc_kappa(a, half_of_b, alpha)
-        theta = self.calc_theta(x, half_of_b, gamma)
-        omega = self.calc_omega(a, half_of_b, kappa)
-        Rx, Tx = degrees((pi/2) - theta), degrees((pi/2) - omega) 
+        alpha = self.calc_alpha(a=a, c=c)
+        beta = self.calc_beta(x=x, y=y)
+        m = self.calc_m(x=x, b=self.half_of_b, beta=beta)
+        n = self.calc_n(a=a, b=self.half_of_b, alpha=alpha)
+        theta = self.calc_theta(x=x, b=self.half_of_b, m=m)
+        omega = self.calc_omega(a=a, b=self.half_of_b, n=n)
+        Rx, Tx = degrees((pi/2) - theta), degrees((pi/2) - omega)
+        if Print_vals:
+            print(f"Tx Dystance a = {a/1000} \nTx Dystance c = {c/1000} \nRx Dystance  x = {x/1000} \nRx Dystance  y = {y/1000}\nTx angle = {Tx} \nRx angle = {Rx}\n\n")
         return Rx, Tx
+    
 
 
 # class Antenna_Geometry():
@@ -92,11 +114,22 @@ class Antenna_Geometry():
 #         print(degrees(alpha))
 #         c = self.calc_c(a, alpha)
 #         beta = self.calc_beta(c, d)
-#         gamma = pi/2 - pi - beta
-#         return gamma
+#         m = pi/2 - pi - beta
+#         return m
     
 
 if __name__ == "__main__":
-    a = Antenna_Geometry(None, None, 0.3)
-    print(degrees(a.calc_angle()))
+    uwb = UWB_module()
+    geo = Antenna_Geometry(uwb, 0.26)
+    while True:
+        try:
+            geo.get_angles(Print_vals=True)
+        except Exception as e:
+            print(e)
+        sleep(1)
+        # if os.name == "nt":
+        #     os.system("cls")
+        # else:
+        #     os.system("clear")
+        
 
--- No_wait_first/get_distances.py
+++ No_wait_first/get_distances.py
@@ -6,46 +6,55 @@ class UWB_module():
         self.uwb_dev = serial.Serial(port, b_rate)
         self.uwb_dev.reset_input_buffer()
         self.uwb_dev.reset_output_buffer()
-        self.uwb_dev.write("deca?") # Nie wiem co to dokładnie robi ale w kodzie źródłowym aplikacji jest
+        #self.uwb_dev.write("deca?") # Nie wiem co to dokładnie robi ale w kodzie źródłowym aplikacji jest
         self.no_of_lines = no_of_lines
         self.max_attempts = max_attempts
 
 
     def get_uwb_data(self):
         #self.uwb_dev.reset_input_buffer()
-        mr = []
+        mc = []
         ma = []
         attempts = 0
         prev_ma_no = "0"
-        prev_mr_no = "0"
+        prev_mc_no = "0"
         while True:
             read = self.uwb_dev.readline().decode('utf-8', errors='ignore').strip()
             read = read.split(" ")
-            if read.startswith('mr') and len(mr) < self.no_of_lines and read[-3] != prev_mr_no:
-                mr.append(read)
-                prev_mr_no = read[-3]
-            elif read.startswith('ma') and len(ma) < self.no_of_lines and read[-3] != prev_ma_no:
+            if read[0]==('mc') and len(mc) < self.no_of_lines and read[-3] != prev_mc_no:
+                mc.append(read)
+                prev_mc_no = read[-3]
+            elif read[0]==('ma') and len(ma) < self.no_of_lines and read[-3] != prev_ma_no:
                 ma.append(read)
                 prev_ma_no = read[-3]
             attempts += 1
-            if (len(mr) == self.no_of_lines and len(ma) == self.no_of_lines):
+            if (len(mc) == self.no_of_lines and len(ma) == self.no_of_lines):
                 break
-        return mr, ma
+        return mc, ma
     
     def process_data(self, data):
         ret_data = [[], [], [], []]
         # [RANGE0[], RANGE1[], RANGE2[], RANGE3[]]
         for datum in data:
             for i in range(4):
-                ret_data[i].append(float('0x' + datum[i+2]))
+                ret_data[i].append(int(datum[i+2],16))
         return ret_data
     
+    def avg(self, data):
+        ret = []
+        for datum in data:
+            dat = sorted(datum)
+            dat = dat[1:-1]
+            dat = average(dat)
+            ret.append(dat)
+        return ret
+
 
     def get_distances(self):
-        mr, ma = self.get_uwb_data()
-        tag_distances_lists, anchor_distances_lists = (self.process_data(mr), self.process_data(ma))
-        calc_dist_tag = [average(sorted(values)[1:-1]) for values in zip(*tag_distances_lists)]
-        calc_dist_anchor = [average(sorted(values)[1:-1]) for values in zip(*anchor_distances_lists)]
+        mc, ma = self.get_uwb_data()
+        tag_distances_lists, anchor_distances_lists = (self.process_data(mc), self.process_data(ma))
+        calc_dist_tag = self.avg(tag_distances_lists)
+        calc_dist_anchor = self.avg(anchor_distances_lists)
         return calc_dist_tag, calc_dist_anchor
         
     
