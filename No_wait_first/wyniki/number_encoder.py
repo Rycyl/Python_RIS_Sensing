d2c56b7e728d5e25657ffaa1b0468270b240bccb
Tomasz Jaworski <132903579+treecodeXXI@users.noreply.github.com>
2025-04-04 13:39:25 +0200

    Rm no_wait_first from main directory

--- No_wait_first/wyniki/number_encoder.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def get_angles(n: str):
-    with open("number_encoder", newline='', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        next(reader)
-        for row in reader:
-            if row[0]==n:
-                return  "Rx=" + row[2] + " Tx=" + row[1]
