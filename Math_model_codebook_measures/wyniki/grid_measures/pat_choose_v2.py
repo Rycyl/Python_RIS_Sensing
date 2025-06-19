98f7894332ab33d2e7961fb8d6a28082c5e190a6
Tomasz Jaworski <132903579+treecodeXXI@users.noreply.github.com>
2025-06-19 14:49:34 +0200

    Implement random select
    in general code needs a little clean up
    select function need to be moved as separate file

--- Math_model_codebook_measures/wyniki/grid_measures/pat_choose_v2.py
+++ Math_model_codebook_measures/wyniki/grid_measures/pat_choose_v2.py
@@ -2,33 +2,6 @@ from class_codebook import *
 from class_measures_result import *
 import numpy as np
 
-
-def load_results_from_file(selected):
-    results = Results()
-    codebook = Codebook()
-    print("source data loaded")
-    currtent_pattern = None
-
-    selected = Selected()
-    i = -49
-    for d in range(0, 90):
-        used_patterns = [0] * 919
-        print("D=", d)
-        selected.selected.append(Select(i,d))
-        for x in codebook.patterns:
-            if used_patterns[x.idx] != 1:
-                for a in x.angles:
-                    if used_patterns[x.idx] == 1:
-                        break
-                    if (a[0] == i and a[1] == d):
-                        selected.selected[-1].add_pat_idx(a[2], x.idx,  results.results[x.idx].powers)
-                        used_patterns[x.idx] = 1
-            else:
-                continue         
-    selected.dump_class_to_file(dumpfile=dumpfile)
-    return selected
-
-
 class Selected:
     def __init__(self):
         self.selected = []
@@ -38,7 +11,7 @@ class Selected:
         # Serializacja obiektu do pliku
         with open(dumpfile, 'wb') as file:
             pickle.dump(self, file)
-        print("Results class dumpted to a file: ", dumpfile)
+        print("Select class dumpted to a file: ", dumpfile)
 
     def load_from_file(self, dumpfile):
         try:
@@ -46,6 +19,7 @@ class Selected:
                 loaded_object = pickle.load(file)
             self.selected = loaded_object.selected
             print("pikle loaded")
+            return
         except:
             pass
 
@@ -76,6 +50,7 @@ class Select:
     
     def find_max(self):
         if self.powers != []:
+            print(self.powers)
             array = np.array(self.powers)
             max_values = np.max(array, axis=0)
             max_indices = np.argmax(array, axis=0)
@@ -84,15 +59,41 @@ class Select:
             self.maxs  = (max_values)
             self.maxs_idx = (max_indices)
         return
-        
+
+
+def load_results_from_file(selected):
+    results = Results()
+    codebook = Codebook()
+    print("source data loaded")
+    currtent_pattern = None
+
+    selected = Selected()
+    i = -49
+    for d in range(0, 90):
+        used_patterns = [0] * len(codebook.patterns)
+        print("D=", d)
+        selected.selected.append(Select(i,d))
+        for x in codebook.patterns:
+            if used_patterns[x.idx] != 1:
+                for a in x.angles:
+                    if used_patterns[x.idx] == 1:
+                        break
+                    if (a[0] == i and a[1] == d):
+                        selected.selected[-1].add_pat_idx(a[2], x.idx,  results.results[x.idx].powers)
+                        used_patterns[x.idx] = 1
+            else:
+                continue         
+    selected.dump_class_to_file(dumpfile=dumpfile)
+    return selected
+
+
+
 
 dumpfile= "wybrane_paterny_pk_metod_v2.pkl"
 # try:
 selected = Selected()
 #selected = load_results_from_file(selected)
 selected.load_from_file(dumpfile=dumpfile)
-#pass
-# try:
 # for k in selected.selected:
 #         k.find_max()
 #         print("\n\n\n")
@@ -134,3 +135,23 @@ print(max_values)
 #     x.find_max()
                     
 # selected.dump_class_to_file(dumpfile=dumpfile)
+import matplotlib.pyplot as plt
+FONTSIZE = 20
+MARKERSIZE = 18
+
+# Use Rx_Angle as the x-axis
+results= Results()
+x_axis = results.results[0].Rx_Angle
+# Create a figure and axis
+plt.figure(figsize=(10, 8))
+
+# Plot ref_pows
+plt.plot(x_axis, max_values,  markersize=MARKERSIZE, label='max in selected', color='blue', marker='X')
+
+# Plot res_best_pows
+plt.xlabel('Kąt położenia odbiornika [stopnie]', fontsize=FONTSIZE)
+plt.ylabel('Wartości mocy odebranej [dBm]', fontsize=FONTSIZE)
+plt.grid()
+plt.legend(fontsize=FONTSIZE, loc='upper right')
+
+plt.show()
