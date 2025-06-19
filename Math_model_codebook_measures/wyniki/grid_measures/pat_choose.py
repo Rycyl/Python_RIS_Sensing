c52ca0c9bbe10dd1e391164bb1601a875015dd0c
Tomasz Jaworski <132903579+treecodeXXI@users.noreply.github.com>
2025-06-19 14:51:45 +0200

    Selected class move as sepaate file

--- Math_model_codebook_measures/wyniki/grid_measures/pat_choose.py
+++ Math_model_codebook_measures/wyniki/grid_measures/pat_choose.py
@@ -1,65 +1,9 @@
 from class_codebook import *
 from class_measures_result import *
+from class_select import *
 import numpy as np
 
 
-
-class Selected:
-    def __init__(self):
-        self.selected = []
-    
-    def dump_class_to_file(self, dumpfile):
-        # Serializacja obiektu do pliku
-        with open(dumpfile, 'wb') as file:
-            pickle.dump(self, file)
-        print("Results class dumpted to a file: ", dumpfile)
-
-    def load_from_file(self, dumpfile):
-        try:
-            with open(dumpfile, 'rb') as file:
-                loaded_object = pickle.load(file)
-            self.selected = loaded_object.selected
-            print("pikle loaded")
-        except:
-            pass
-
-class Select:
-    def __init__(self,i,d):
-        self.i = i
-        self.d = d
-        self.s = []
-        self.pat_idx = []
-        self.powers = []
-
-    def add_pat_idx(self, s, idx, pows):
-        self.s.append(s)
-        self.pat_idx.append(idx)
-        self.powers.append(pows)
-
-    def show(self):
-        print("i = ", self.i, " d = ", self.d)
-        for j in range(len(self.s)):
-            print(f"For shift = {self.s[j]}, pat_idx = {self.pat_idx[j]}")
-    
-    def show_pows(self):
-        print("i = ", self.i, " d = ", self.d)
-        for x in self.powers:
-            print([f"{value:.3f}" for value in x])
-    
-    def find_max(self):
-        if self.powers != []:
-            array = np.array(self.powers)
-            max_values = np.max(array, axis=0)
-            max_indices = np.argmax(array, axis=0)
-            print(max_values)
-            print(max_indices)
-            for l in range(len(max_indices)):
-                max_indices[l] = self.pat_idx[max_indices[l]]
-            print(max_indices)
-            self.show_pows()
-            self.show()
-        return
-        
 
 dumpfile= "wybrane_paterny_pk_metod.pkl"
 # try:
