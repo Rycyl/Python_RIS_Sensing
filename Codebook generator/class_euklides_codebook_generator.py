e060094de52e64c392cdb7db64fa81f71b6526cb
Tomasz Jaworski <132903579+treecodeXXI@users.noreply.github.com>
2025-12-15 14:03:14 +0100

    Update class_euklides_codebook_generator.py
    Add method of generating euklides codebook from larger codebook/set of patterns

--- Codebook generator/class_euklides_codebook_generator.py
+++ Codebook generator/class_euklides_codebook_generator.py
@@ -1,14 +1,23 @@
 import csv
 import threading
 import os
+import random
 
 import numpy as np
 from bitstring import BitArray
+
 from class_codebook import Codebook, Pattern
+
 
 def pattern_hamming_distance(a, b):
     c = a ^ b
     return c.count(1)
+
+def get_patterns_from_codebook(codebook):
+    patterns_from_codebook = []
+    for pat in codebook.patterns:
+        patterns_from_codebook.append(pat.pattern)
+    return patterns_from_codebook
 
 class Euklides_codebook():
     def __init__(self, Q, i):
@@ -17,9 +26,9 @@ class Euklides_codebook():
         return
 
     def generate_random_bitarray(self, length=16):
-        # Generowanie losowego bitu jako tablicy numpy
+        # gen random numpy bits of len=lenght
         random_bits = np.random.choice([0, 1], size=length)
-        # Tworzenie BitArray z losowych bitów
+        # make bitarray from random_bits
         return BitArray(bin=''.join(map(str, random_bits)))
 
     def calculate_metric(self, patterns, i, pat=None):
@@ -39,11 +48,26 @@ class Euklides_codebook():
         return ret_val
 
 
-    def generate_codebook(self):
-        codebook = Codebook(do_load=False)
+    def generate_codebook(self, Q=None, i_bound=None, k_bound=None):
+        """
+            Generates a codebook based on a previously generated codebook 
+                using the Euclidean (Hamming) method.
+            Q: the number of RIS elements (default = 16 [columns])
+            i_bound: the number of iterations for the algorithm
+            k_bound: the limit on searching for a new better candidate;
+                    if exceeded - break
+            Returns a Codebook() object
+        """
+        if Q != None:
+            self.Q = Q
+        if i_bound != None:
+            self.i_bound = i_bound
+        if k_bound == None:
+            k_bound = 2147483646 #int32 size -1
+
         patterns = []
         metrics = []
-        
+
         #inicjalizacja algorytmu
         for i in range(self.Q):
             patterns.append(self.generate_random_bitarray(16))
@@ -65,23 +89,106 @@ class Euklides_codebook():
                 i = i + 1
                 k = 0
             k+=1
-            if k>10000:
+            if k>k_bound:
                 break
-        
+
+        #put patterns into codebook object
+        codebook = Codebook(do_load=False)
         for i,pattern in enumerate(patterns):
             codebook.add_pattern(pattern=Pattern(idx=i, pattern=(pattern*16).hex))
 
         return codebook
 
+
+    def generate_codebook_from_codebook(self, bigger_codebook, Q=None, i_bound=None, k_bound=None):
+        """
+            Generates a codebook based on a previously generated codebook 
+                using the Euclidean (Hamming) method.
+            bigger_codebook: previously generated codebook
+            Q: the number of RIS elements (default = 16 [columns])
+            i_bound: the number of iterations for the algorithm
+            k_bound: the limit on searching for a new better candidate;
+                    if exceeded - break
+            Returns a Codebook() object
+        """
+        patterns_from_codebook = get_patterns_from_codebook(bigger_codebook)
+
+        patterns = []
+        metrics = []
+        
+        if Q != None:
+            self.Q = Q
+        if i_bound != None:
+            self.i_bound = i_bound
+        if k_bound == None:
+            k_bound = (len(patterns_from_codebook)-Q) * 50 #50 times a number of posibilites
+
+        #select Q random patterns from given codebook
+        for i in range(self.Q): 
+            pat = random.choice(patterns_from_codebook)
+            patterns.append(pat)
+
+
+        #count metrics
+        for i,pattern in enumerate(patterns):
+            metrics.append(self.calculate_metric(patterns, i))
+        
+        #main loop
+        i = 0
+        k = 0
+        while i < self.i_bound:
+            print("i,k", i, k)
+            min_index = metrics.index(min(metrics))
+            #select non selected patter from codebook
+            not_done = True
+            while not_done:
+                new_pattern = random.choice(patterns_from_codebook)
+                if new_pattern in patterns:
+                    new_pattern = random.choice(patterns_from_codebook)
+                else:
+                    not_done = False
+
+            new_metric = self.calculate_metric(patterns, min_index, pat=new_pattern)
+            if new_metric>metrics[min_index]:
+                patterns[min_index] = new_pattern
+                metrics[min_index]  = new_metric
+                i = i + 1
+                k = 0
+            k+=1
+            if k>k_bound:
+                break
+        
+        codebook = Codebook(do_load=False)
+        for i,pattern in enumerate(patterns):
+            codebook.add_pattern(pattern=Pattern(idx=i, pattern=pattern.hex))
+
+        return codebook
+
 if __name__=="__main__":
-    euklides_codebook = Euklides_codebook(100, 1000)
-    e_codebook = euklides_codebook.generate_codebook()
-    print(e_codebook)
-    dumpname = "codebooks\euklides_codebook.pkl"
-    e_codebook.dump_class_to_file(dumpfile=dumpname)
-    pass
-    
-    test_load = Codebook(dumpfile=dumpname, filename=None, do_load=True)
+    # euklides_codebook = Euklides_codebook(64, 640)
+    # try:
+    #     e_codebook_64 = Codebook(dumpfile="codebooks/euklides_codebook64.pkl")
+    # except:
+    #     e_codebook_64 = euklides_codebook.generate_codebook(Q=64)
+    #     e_codebook_64.dump_class_to_file(dumpfile="codebooks/euklides_codebook64.pkl")
+    #     print("codebook 64 dumpted")
+    # e_codebook_32 = euklides_codebook.generate_codebook(Q=32, i_bound=480)
+    # e_codebook_32.dump_class_to_file(dumpfile="codebooks/euklides_codebook32.pkl")
+    # # e_codebook_16 = euklides_codebook.generate_codebook(Q=16)
+    # e_codebook_32_from = euklides_codebook.generate_codebook_from_codebook(e_codebook_64, Q=32, i_bound=100, k_bound=1000)
+    # e_codebook_32_from.dump_class_to_file(dumpfile="codebooks/euklides_codebook32_from_64.pkl")
+    # # e_codebook_16_from = euklides_codebook.generate_codebook_from_codebook(e_codebook_64, Q=16)
+
+    # patterns64 = get_patterns_from_codebook(e_codebook_64)
+    # patterns32 = get_patterns_from_codebook(e_codebook_32)
+    # patterns32_from = get_patterns_from_codebook(e_codebook_32_from)
+    # # patterns16 = get_patterns_from_codebook(e_codebook_16)
+    # # patterns16_from = get_patterns_from_codebook(e_codebook_16_from)
+
+    from codebook_analyze import plot_codebook_reduce_by_hamming
+    plot_codebook_reduce_by_hamming(dumpfile="codebooks/euklides_codebook32.pkl")
+    plot_codebook_reduce_by_hamming(dumpfile="codebooks/euklides_codebook32_from_64.pkl")
+
     pass
     pass
 
