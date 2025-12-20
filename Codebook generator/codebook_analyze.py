9e386daae3f0be2271da400f178819f489578881
Tomasz Jaworski <132903579+treecodeXXI@users.noreply.github.com>
2025-12-20 13:15:08 +0100

    Update codebook_analyze.py
    Bugfix of def AF_from_pattern()
    
    Add def plot_codebooks_AFs()
    
    Add def  reduce_codebook_by_hamming()
    Add method plot_codebooks_reduce_by_hamming()

--- Codebook generator/codebook_analyze.py
+++ Codebook generator/codebook_analyze.py
@@ -5,6 +5,7 @@ from bitstring import BitArray
 from class_codebook import Codebook
 import matplotlib.cm as cm
 import copy
+from matplotlib.cm import get_cmap
 
 def ris_x_distance(m, x_ris=0.02):
     return ((x_ris / 2) + (m * x_ris))
@@ -29,44 +30,6 @@ def sin(alfa):
 
 def cos(alfa):
     return np.cos(radians(alfa))
-
-def plot_codebook_reduce_by_hamming(dumpfile, FONTSIZE=16):
-    codebook_instance = Codebook(dumpfile=dumpfile)
-    codebook_patterns = []
-    for pattern in codebook_instance.patterns:
-        codebook_patterns.append(pattern.pattern)
-
-    codebooks_haming=[]
-    for distance in range(0,16):
-        #print(distance)
-        if distance==0:
-            codebooks_haming.append(codebook_patterns)
-        else:
-            codebooks_haming.append([codebook_patterns[0]])
-            for pattern in codebook_patterns:
-                haming_min = 20
-                for current_codebook_pattern in codebooks_haming[-1]:
-                    haming = hamming_distance(pattern, current_codebook_pattern)/16
-                    if haming < haming_min:
-                        haming_min = copy.copy(haming)
-                if haming_min > distance:
-                    codebooks_haming[-1].append(pattern)
-        #print(codebooks_haming[-1])
-        #print("\n\n\n")
-    pat_no = []
-    for x in codebooks_haming:
-        pat_no.append(len(x))
-    print(pat_no)
-    # Create a simple line plot
-
-    plt.rcParams['font.size'] = FONTSIZE
-    plt.plot(pat_no, marker='o', linewidth=4, markersize=8)  # 'o' adds markers to the data points
-    plt.xlabel('Minimum Hamming Distance Between Patterns in Codebook')
-    plt.ylabel('Number of Unique Patterns in Codebook')
-    # plt.yscale('log')
-    plt.grid(True)
-    plt.show()
-
 
 def AF_from_pattern(pattern, theta_Tx, phi_Tx=0, silent = False):
     """
@@ -102,19 +65,20 @@ def AF_from_pattern(pattern, theta_Tx, phi_Tx=0, silent = False):
     if not silent:
         print("AF for pattern: ", pattern.hex)
     
-    y_n = 7
+    y_n = 0
     #RIS_element_index = 0
-    while y_n >= -8:
-        x_m = -8
-        while x_m < 8:
+    while y_n < 16:
+        x_m = 0
+        while x_m < 16:
             kat = -90
-            RIS_element_index = ((y_n+8) * (x_m+8)) + x_m + 8
+            RIS_element_index = (y_n * 16) + x_m
+            #print(RIS_element_index)
             while kat < 90:
                 AFs[kat+90]+=(AF_single(x_m, y_n, pattern[RIS_element_index], kat, 0, theta_Tx=theta_Tx))
                 kat += 1
             x_m += 1
             #end while x_m
-        y_n -= 1
+        y_n += 1
         #end while y_n
     abs_AF = []
     for x in AFs:
@@ -127,9 +91,9 @@ def AF_from_pattern(pattern, theta_Tx, phi_Tx=0, silent = False):
 
 def plot_AFs(AFs):
     """
-    Plot Array Factors
-    Inputs:
-    AFs : a list of array factors: [[AF1],[AF2],[AF3]....]
+        Plot Array Factors
+        Inputs:
+        AFs : a list of array factors: [[AF1],[AF2],[AF3]....]
     """
     plt.figure(figsize=(10, 6))
     x = range(-90,90)
@@ -138,7 +102,108 @@ def plot_AFs(AFs):
     plt.xticks(np.arange(-90, 91, 10))
     plt.yticks(np.arange(0, 30, 5))
     plt.xlim(-90, 90)
+    plt.ylim(0, 25)
     plt.xlabel("Rx location [°]")
     plt.ylabel("AF [dB]")
     plt.grid()
+    plt.show()
+
+def plot_codebooks_AFs(codebooks_AFs, labels):
+    """
+        Plot Array Factors
+        Inputs:
+        AFs : a list of array factors of codebooks: [ [ [AF1],...,[AFn] ], [ [AF1],...,[AFn] ] ]
+                            so also like that:    [ [ codebook 1      ], [ codebook 2      ] ]
+    """
+    colors = get_cmap('tab10')
+    plt.figure(figsize=(10, 6))
+    x = range(-90,90)
+    for i,AFs in enumerate(codebooks_AFs):
+        for AF in AFs:
+            plt.plot(x, AF, color=colors(i % 10))
+        plt.plot([], [], label=labels[i], color=colors(i % 10))  #empty plt for label
+    plt.xticks(np.arange(-90, 91, 10))
+    plt.yticks(np.arange(0, 30, 5))
+    plt.xlim(-90, 90)
+    plt.ylim(0, 25)
+    plt.xlabel("Rx location [°]")
+    plt.ylabel("AF [dB]")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+def reduce_codebook_by_hamming(codebook):
+    """
+        Limits the size of a codebook based on Hamming distances between patterns.
+
+        This function iterates through the patterns in the provided 
+        codebook and builds a series of codebooks, each corresponding 
+        to a specified maximum Hamming distance. The first pattern 
+        is always included, and subsequent patterns are added only if 
+        they satisfy the distance constraint relative to the already 
+        included patterns.
+
+        Inputs:
+        - codebook: Codebook object.
+
+        Returns:
+        - codebooks_haming: A LIST of codebooks, each consisting of patterns that 
+                            adhere to the allowable Hamming distance constraints. 
+                            The length of this list corresponds to the range of 
+                            distance limits from 0 to 16.
+
+        TO DO:
+            maybe change returns to Codebook() obj.
+    """
+    codebook_patterns=[]
+    for pattern in codebook.patterns:
+        codebook_patterns.append(pattern.pattern)
+
+    codebooks_haming=[]
+    for distance_lim in range(0,17):
+        print(distance_lim)
+        if distance_lim==0:#0 distance lim are all patterns
+            codebooks_haming.append(codebook_patterns)
+        else:
+            codebooks_haming.append([codebook_patterns[0]])
+            for pattern in codebook_patterns: #loop in all patterns
+                hamming_min = 1024
+                for current_codebook_pattern in codebooks_haming[-1]: #loop current selections
+                    haming = hamming_distance(pattern, current_codebook_pattern)/16 #get current pat distance
+                    if haming < hamming_min: #if smalller - store
+                        haming_min = haming
+                if haming > distance_lim: # if min distance to others > limit distance - add to codebook
+                        codebooks_haming[-1].append(pattern)
+        #print(codebooks_haming[-1])
+        #print("\n\n\n")
+    pat_no = []
+    for x in codebooks_haming:
+        pat_no.append(len(x))
+    print(pat_no)
+
+    return codebooks_haming
+
+def plot_codebooks_reduce_by_hamming(codebooks_list, FONTSIZE=16):
+    """
+        Plots a few codebooks (in list format) sizes, from lists 
+        returned by reduce_codebook_by_hamming().
+
+        input:
+        codebooks_list: list of lists from reduce_codebook_by_hamming()
+        FONTSIZE: font size for plot :O
+    """
+    plt.plot()
+    
+    for data,label in codebooks_list:
+        pat_no = []
+        for y in data:
+            pat_no.append(len(y))
+        plt.plot(pat_no, marker='o', linewidth=4, markersize=8, label=label)  # 'o' adds markers to the data points
+    plt.xlabel('Minimum Hamming Distance Between Patterns in Codebook')
+    plt.ylabel('Number of Unique Patterns in Codebook')
+    # plt.yscale('log')
+    plt.legend()
+    plt.rcParams['font.size'] = FONTSIZE
+    plt.grid(True)
     plt.show()
