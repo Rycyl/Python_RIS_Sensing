cbc5ea13576b0b9fcc7268489d36a7c05459b7ba
Tomasz Jaworski <djaboloo@gmail.com>
2026-04-22 14:48:36 +0200

    modify plot programs in measurements 17_04

---
+++ Meas_codebooks_17_04_26/wyniki/plots.py
@@ -0,0 +1,272 @@
+
+from class_measures_result import Results
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import math
+from class_codebook import *
+import copy
+
+def power_in_position(dumpfile):
+    results = Results(dumpfile=dumpfile)
+
+    powers   = []
+
+
+
+    for result in results.results:
+        powers.append(result.powers)
+    #change axises
+    powers_np_array = np.array(powers)
+    data = powers_np_array.T 
+
+    # Uzyskaj nazwę folderu, w którym znajduje się skrypt
+    folder_name = os.path.dirname(os.path.abspath(__file__))
+    plots_folder = os.path.join(folder_name, 'power_in_position')
+
+    # Utwórz folder "plots", jeśli nie istnieje
+    os.makedirs(plots_folder, exist_ok=True)
+
+    # Rysowanie każdego wiersza na osobnym wykresie i zapisywanie do plików
+    for i in range(data.shape[0]):
+        plt.figure()  # Utwórz nową figurę dla każdego wykresu
+        plt.plot(data[i], label=f'Wiersz {i+1}')
+        plt.title(f'Tx {int(results.results[0].Tx_Angle[i])}, Rx {int(results.results[0].Rx_Angle[i])}')
+        plt.xlabel('N-ty_pattern')
+        plt.ylabel('Moc [dBm]')
+        plt.ylim(-85, -50)  # Ustawienie stałego zakresu osi Y
+        plt.grid(True)  # Włączenie linii pomocniczych
+        plt.legend()
+        
+        # Zapisz wykres do pliku w folderze "plots"
+        plt.savefig(os.path.join(plots_folder, f'{dumpfile}_wykres_wiersz_{i+1}.png'))
+        plt.close()  # Zamknij figurę, aby nie pokazywać podglądu
+
+def plot_pow_in_pos_teams(dumpfile):
+    results = Results(dumpfile=dumpfile)
+
+
+    def dbm_to_mw(x):
+        mW=10**(x/10)
+        return mW
+    def mw_to_dbm(x):
+        dbm=10*math.log10(x)
+        return dbm
+
+    powers   = []
+    #wez wartości z metody opt kolumnami i wylicz które to min/max
+    ma1 = (results.maxs[-3].powers)
+    ma2 = (results.maxs[-2].powers)
+    mi1 = (results.mins[-3].powers)
+    mi2 = (results.mins[-2].powers)
+
+    maxs = []
+    for i in range(len(ma1)):
+        if ma1[i]>ma2[i]:
+            maxs.append(ma1[i])
+        else:
+            maxs.append(ma2[i])
+
+    mins = []
+    for i in range(len(mi1)):
+        if mi1[i]<mi2[i]:
+            mins.append(mi1[i])
+        else:
+            mins.append(mi2[i])
+    #############################
+    #codebook zeby wybrac tylko patterny z niego
+    codebook_tx48=Codebook(dumpfile='Codebook_tx48.pkl')
+    for result in results.results:
+        for pat in codebook_tx48.patterns:
+                if pat.pattern == result.pattern:
+                    pow = result.powers
+                    powers.append(pow)
+    print(maxs, mins)
+    #change axis
+    powers_np_array = np.array(powers)
+    data = powers_np_array.T #transpose
+
+    # Uzyskaj nazwę folderu, w którym znajduje się skrypt
+    folder_name = os.path.dirname(os.path.abspath(__file__))
+    plots_folder = os.path.join(folder_name, 'power_in_position_teams_rework')
+
+    # Utwórz folder "plots", jeśli nie istnieje
+    os.makedirs(plots_folder, exist_ok=True)
+    FONTSIZE=16
+    # Rysowanie każdego wiersza na osobnym wykresie i zapisywanie do plików
+    for i in range(data.shape[0]):
+        
+        dat = np.sort((data[i]))
+        wat_dat = []
+        for x in dat:
+            wat_dat.append(dbm_to_mw(x))
+        avg = np.mean(wat_dat)
+        avg = mw_to_dbm(avg)
+        plt.figure()  # Utwórz nową figurę dla każdego wykresu
+        plt.rcParams['font.size'] = FONTSIZE
+        plt.rcParams['lines.linewidth']= 3
+        plt.plot(dat, color='royalblue', label=f'Full codebook entries')
+        plt.axhline(y=maxs[i], color='orangered', linestyle='--', label='SC max')
+        plt.axhline(y=avg, color='cyan', linestyle='--', label='Linear average')
+        plt.axhline(y=mins[i], color='violet', linestyle='--', label='SC min')
+        #plt.title(f'Rx at {int(results.results[0].Rx_Angle[i])}°')
+        plt.xlabel('N\'th sorted pattern in recieved power')
+        plt.ylabel('Recieved power [dBm]')
+        plt.ylim((min(mins)//5)*5, ((max(maxs)//5)+2)*5)  # Ustawienie stałego zakresu osi Y
+        plt.grid(True)  # Włączenie linii pomocniczych
+        plt.margins()
+        plt.legend(loc='lower right',  markerscale=4)
+        plt.subplots_adjust(left=0.16, right=0.9, top=0.95, bottom=0.16, wspace=0.2, hspace=0.2)
+        # plt.show()
+        # Zapisz wykres do pliku w folderze "plots"
+        save_format='svg'
+        plt.savefig(os.path.join(plots_folder, f'{dumpfile}_wykres_{i+1}_pow_sort_rx_{int(results.results[0].Rx_Angle[i])}.{save_format}'), format=save_format)
+        plt.close()  # Zamknij figurę, aby nie pokazywać podglądu
+
+def plot_pow_in_pos_merge(dumpfile):
+    results = Results(dumpfile=dumpfile)
+
+    powers   = []
+
+
+
+    for result in results.results:
+        powers.append(result.powers)
+    #change axises
+    powers_np_array = np.array(powers)
+    data = powers_np_array.T 
+
+    # Uzyskaj nazwę folderu, w którym znajduje się skrypt
+    folder_name = os.path.dirname(os.path.abspath(__file__))
+    plots_folder = os.path.join(folder_name, 'power_in_position')
+
+    # Utwórz folder "plots", jeśli nie istnieje
+    os.makedirs(plots_folder, exist_ok=True)
+    plt.figure(figsize=(15, 10))
+    # Plot each row
+    for i in range(data.shape[0]):
+        plt.plot(data[i], label=f'Tx {int(results.results[0].Tx_Angle[i])}, Rx {int(results.results[0].Rx_Angle[i])}')
+
+    # Add labels and title
+    plt.xlabel('Pattern ID')
+    plt.ylabel('Power')
+    plt.title('Power for patterns')
+    plt.legend()
+    plt.grid(True)
+        
+    # Zapisz wykres do pliku w folderze "plots"
+    plt.savefig(os.path.join(plots_folder, f'{dumpfile}_plot.png'))
+    plt.close()  # Zamknij figurę, aby nie pokazywać podglądu
+
+def plot_pattern_characteristics(dumpfile):
+    results = Results(dumpfile=dumpfile)
+
+    # Create the directory if it doesn't exist
+    output_directory = 'pattern_characteristics'
+    os.makedirs(output_directory, exist_ok=True)
+
+    # Assuming results is a list of objects with attributes powers, Rx_Angle, and idx
+    for result in results.results:
+        powers = result.powers
+        rx = result.Rx_Angle
+        idx = result.idx
+        print("plotting.... ", idx)
+        # Combine rx and powers into a list of tuples and sort by rx
+        sorted_pairs = sorted(zip(rx, powers))
+        
+        # Unzip the sorted pairs back into sorted rx and powers
+        sorted_rx, sorted_powers = zip(*sorted_pairs)
+
+        # Create a new figure
+        plt.figure()
+        
+        # Scatter plot
+        plt.scatter(sorted_rx, sorted_powers, color='blue', label='Data Points')
+        
+        # Line plot to connect the dots
+        plt.plot(sorted_rx, sorted_powers, color='orange', linestyle='-', label='Connecting Line')
+        
+        # Set the title with the id
+        plt.title(f'Result ID: {idx}')
+        
+        # Set labels
+        plt.xlabel('Rx Angle')
+        plt.ylabel('Power')
+        
+        # Set y-axis limits
+        plt.ylim(-85, -50)
+        
+        # Add a legend
+        plt.legend()
+        
+        # Show the grid
+        plt.grid()
+        
+        # Save the plot to the specified directory
+        plt.savefig(os.path.join(output_directory, f'{dumpfile}_plot_{idx}.png'))
+        
+        # Close the figure to free up memory
+        plt.close()
+
+
+def hamming_distance(a, b):
+    c = a ^ b
+    return c.count(1)
+
+def dbm_to_mw(x):
+    mW=10**(x/10)
+    return mW
+def mw_to_dbm(x):
+    dbm=10*math.log10(x)
+    return dbm
+
+
+def plot_hamming(dumpfile):
+    results = Results(dumpfile=dumpfile)
+    powers   = []
+    patterns = []
+
+    max_pat = results.maxs[-2].pattern
+    min_pat = results.mins[-2].pattern
+
+    for result in results.results:
+        if result.idx < 1000:
+            patterns.append(result.pattern)
+            pow = result.powers
+            powers.append(pow)
+    print(len(patterns))
+    #change axis
+    powers_np_array = np.array(powers)
+    data = powers_np_array.T #transpose
+
+    # Uzyskaj nazwę folderu, w którym znajduje się skrypt
+    folder_name = os.path.dirname(os.path.abspath(__file__))
+    plots_folder = os.path.join(folder_name, 'hamming')
+
+    # Utwórz folder "plots", jeśli nie istnieje
+    os.makedirs(plots_folder, exist_ok=True)
+    FONTSIZE=16
+    # Rysowanie każdego wiersza na osobnym wykresie i zapisywanie do plików
+    for i in range(data.shape[0]):
+        sorted_indices = None
+        pats = copy.deepcopy(patterns)
+        sorted_indices = np.argsort(data[i])  
+        pats = [pats[j] for j in sorted_indices]
+        hamming_distances = []
+        for x in pats:
+            hamming_distances.append(hamming_distance(x, max_pat[i])/16)
+        plt.figure()  # Utwórz nową figurę dla każdego wykresu
+        plt.rcParams['font.size'] = FONTSIZE
+        plt.plot(hamming_distances, label=f'Wiersz {i+1}')
+        
+        # plt.title(f'Tx {int(results.results[0].Tx_Angle[i])}, Rx {int(results.results[0].Rx_Angle[i])}')
+        plt.xlabel('N\'th sorted pattern in recieved power')
+        plt.ylabel('Hamming distance to opt best')
+        plt.ylim(0, 16)  # Ustawienie stałego zakresu osi Y
+        plt.grid(True)  # Włączenie linii pomocniczych
+        #plt.legend()
+        plt.subplots_adjust(left=0.16, right=0.9, top=0.95, bottom=0.16, wspace=0.2, hspace=0.2)
+        # Zapisz wykres do pliku w folderze "plots"
+        save_format='svg'
+        plt.savefig(os.path.join(plots_folder, f'{dumpfile}_plot{i+1}_hamming_rx_{int(results.results[0].Rx_Angle[i])}.{save_format}'))
+        plt.close()  # Zamknij figurę, aby nie pokazywać podglądu        
